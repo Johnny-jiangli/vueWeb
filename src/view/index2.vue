e44bb9d219f6303f4743ad405f4760153d591bd4
johnny-jiangli <jiangli450324@163.com>
2018-07-20 16:39:10 +0800

    路由对应的视图

--- src/view/index2.vue
+++ src/view/index2.vue
@@ -1,29 +1,29 @@
 <template>
   <div id="">
     <site-index-navigation-bar></site-index-navigation-bar>
-    <site-main></site-main>
+    <site-main-b></site-main-b>
   </div>
 </template>
 
 <script>
-  import SiteMain from '../components/siteMain'
+  import SiteMainB from '../components/siteMainB'
   import SiteIndexNavigationBar from '../components/basis/siteIndexNavigationBar'
-    export default {
-        name: "index",
-      components:{
-          SiteMain,
-        SiteIndexNavigationBar
-      }
+  export default {
+    name: "index",
+    components:{
+      SiteMainB,
+      SiteIndexNavigationBar
     }
+  }
 </script>
 
 <style scoped>
-/*div{*/
+  /*div{*/
   /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   /*-webkit-font-smoothing: antialiased;*/
   /*-moz-osx-font-smoothing: grayscale;*/
   /*text-align: center;*/
   /*!*background: #465484;*!*/
   /*color: white;*/
-/*}*/
+  /*}*/
 </style>
---
+++ src/view/testPanel.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "test-panel"
+    }
+</script>
+
+<style scoped>
+
+</style>
