66c69fcae0779a8f5d9823d3264918d6c37c7e77
johnny-jiangli <jiangli450324@163.com>
2018-07-20 16:38:14 +0800

    增加路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Index from '../view/index'
+import Index2 from '../view/index2'
+
 
 
 Vue.use(Router)
@@ -17,6 +19,10 @@ export default new Router({
       path:'/test',
       name:"Index",
       component:Index
+    },{
+    path:'/test2',
+      name:'Index2',
+      component:Index2
     }
 
   ]
